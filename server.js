254339ff5229a3a712899ec3f7aad0c08e54fe69
Davi Horner Hoe de Castro <davihhcastro@gmail.com>
2022-08-16 14:56:55 -0300

    fix heroku 2.0

--- server.js
+++ server.js
@@ -34,6 +34,5 @@ app.post('/nota', (req, res) => {
 })
 
 // start the server listening for requests
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
